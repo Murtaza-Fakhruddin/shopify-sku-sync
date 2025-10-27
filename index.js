ad61926c9327fe7fac18d5a938cb865986ff6256
Murtaza Fakhruddin <murtaza.webdeveloper@gmail.com>
2025-10-27 22:56:16 +0500

    Port Update

--- index.js
+++ index.js
@@ -347,5 +347,5 @@ app.get('/', (req, res) =>
   res.send('Shopify SKU inventory sync app running (GraphQL bulk version)')
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
